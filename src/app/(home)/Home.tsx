db161fa757fd4c0611fe16831c66fe579517e3d2
Rojan Rajbhandari <rrojan20@gmail.com>
2025-04-23 15:12:30 +0545

    hotfix: change homepage text

--- src/app/(home)/Home.tsx
+++ src/app/(home)/Home.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <div className="w-full h-screen flex items-center justify-center">
-      Home page
+      Home page for Quickbooks Sync
     </div>
   )
 }
