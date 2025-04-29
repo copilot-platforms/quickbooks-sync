5c5d8cb51c99d307946f82616e725b8ec640b0e5
SandipBajracharya <sndp106.sb@gmail.com>
2025-04-29 10:58:34 +0545

    - removed unwanted files

--- src/app/api/quickbooks/log/route.ts
+++
@@ -1,4 +0,0 @@
-import { withErrorHandler } from '@/app/api/core/utils/withErrorHandler'
-import { storeQbConnectionLog } from '@/app/api/quickbooks/log/log.controller'
-
-export const POST = withErrorHandler(storeQbConnectionLog)
