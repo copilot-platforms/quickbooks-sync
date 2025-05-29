b3198f96f9d1047b603450f4404a14d25c48ce2a
SandipBajracharya <sndp106.sb@gmail.com>
2025-05-29 10:11:23 +0545

    fix (OUT 1796): responsive fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={[inter.className].join(' ')}>
-        <div className="layout-container h-screen px-[100] lg:px-[220] pt-6">
+        <div className="layout-container px-8 sm:px-[100] lg:px-[220] pt-6">
           {children}
         </div>
       </body>
