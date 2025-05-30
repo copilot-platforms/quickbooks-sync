97cf1092a6f77ca66dc0557c634975c96ff8ff8a
SandipBajracharya <sndp106.sb@gmail.com>
2025-05-30 10:04:22 +0545

    fix (OUT-1796): minor design fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={[inter.className].join(' ')}>
-        <div className="layout-container px-8 sm:px-[100] lg:px-[220] pt-6">
+        <div className="layout-container px-8 sm:px-[100] lg:px-[220] pb-[54] pt-6">
           {children}
         </div>
       </body>
