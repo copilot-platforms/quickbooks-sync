cecfcf1f9c18904f770d0f4d3dd3ed1e889570ff
SandipBajracharya <sndp106.sb@gmail.com>
2025-06-04 17:48:50 +0545

    feat(OUT 1807): Sync enable feature
    - add a column “is_enabled” flag in the sync table.
    - check sync enable flag whenever webhook (for invoice, product) is triggered

--- src/app/api/quickbooks/product/product.controller.ts
+++ src/app/api/quickbooks/product/product.controller.ts
@@ -29,7 +29,7 @@ export async function storeProductMap(req: NextRequest) {
 export async function getItemsFromQB(req: NextRequest) {
   const user = await authenticate(req)
   const authService = new AuthService(user)
-  const qbTokenInfo = await authService.getQBToken(user.workspaceId)
+  const qbTokenInfo = await authService.getQBToken(user.workspaceId, true)
   if (!qbTokenInfo || !qbTokenInfo.accessToken) {
     throw new APIError(
       httpStatus.UNAUTHORIZED,
