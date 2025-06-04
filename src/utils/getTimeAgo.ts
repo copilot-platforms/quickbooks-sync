511611352b721c98332cf3f66f5b3f54501d5a0d
arpandhakal <arpandhakal20@gmail.com>
2025-06-04 15:47:42 +0545

    fix(OUT-1807) : added a mechanism remove the char Z in date string in getTimeAgo util to avoid timezone issues

--- src/utils/getTimeAgo.ts
+++ src/utils/getTimeAgo.ts
@@ -7,5 +7,6 @@ const timeAgo = new TimeAgo('en-US')
 
 export const getTimeAgo = (date: Date | string | null): string | null => {
   if (!date) return null
-  return timeAgo.format(new Date(date))
+  const dateStr = typeof date === 'string' ? date.replace('Z', '') : date // Remove 'Z' if present to avoid timezone issues.
+  return timeAgo.format(new Date(dateStr))
 }
