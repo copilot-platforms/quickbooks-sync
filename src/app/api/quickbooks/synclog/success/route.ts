cfda6eaa08330a224625866571b970f7871149f5
arpandhakal <arpandhakal20@gmail.com>
2025-06-25 17:14:47 +0545

    chore(OUT-1886) : some random thing

--- src/app/api/quickbooks/synclog/success/route.ts
+++
@@ -1,4 +0,0 @@
-import { withErrorHandler } from '@/app/api/core/utils/withErrorHandler'
-import { getLatestSyncSuccessLog } from '@/app/api/quickbooks/syncLog/syncLog.controller'
-
-export const GET = withErrorHandler(getLatestSyncSuccessLog)
