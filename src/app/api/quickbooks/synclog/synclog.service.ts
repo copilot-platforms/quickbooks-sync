9e3d4c104949ad121a286acf4611e0d0e150fb28
arpandhakal <arpandhakal20@gmail.com>
2025-06-25 13:40:44 +0545

    fix(OUT-1886) : added a fallback to fetch latest successful connection log if no sync log is found

--- src/app/api/quickbooks/synclog/synclog.service.ts
+++ src/app/api/quickbooks/synclog/synclog.service.ts
@@ -1,13 +1,14 @@
 import { BaseService } from '@/app/api/core/services/base.service'
 import { QBSyncLogSelectSchemaType } from '@/db/schema/qbSyncLogs'
 import { LogStatus } from '@/app/api/core/types/log'
+import { ConnectionStatus } from '@/db/schema/qbConnectionLogs'
 
 export class SyncLogService extends BaseService {
   async getLatestSyncSuccessLog(): Promise<Pick<
     QBSyncLogSelectSchemaType,
     'updatedAt'
   > | null> {
-    const log = await this.db.query.QBSyncLog.findFirst({
+    let log = await this.db.query.QBSyncLog.findFirst({
       where: (logs, { eq, and }) =>
         and(
           eq(logs.portalId, this.user.workspaceId),
@@ -18,6 +19,21 @@ export class SyncLogService extends BaseService {
         updatedAt: true,
       },
     })
+
+    //fetch latest successful connection log if no sync log found
+    if (!log) {
+      log = await this.db.query.QBConnectionLogs.findFirst({
+        where: (logs, { eq, and }) =>
+          and(
+            eq(logs.portalId, this.user.workspaceId),
+            eq(logs.connectionStatus, ConnectionStatus.SUCCESS),
+          ),
+        orderBy: (logs, { desc }) => [desc(logs.createdAt)],
+        columns: {
+          updatedAt: true,
+        },
+      })
+    }
     return log || null
   }
 }
