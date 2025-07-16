f6bc4e298a63823f5b47b769edfd44d36e5ef973
SandipBajracharya <sndp106.sb@gmail.com>
2025-07-16 18:27:20 +0545

    fix(OUT-2025): update zod validation

--- src/type/dto/webhook.dto.ts
+++ src/type/dto/webhook.dto.ts
@@ -33,7 +33,7 @@ export const InvoiceCreatedResponseSchema = z.object({
     status: z.nativeEnum(InvoiceStatus),
     total: z.number(),
     taxAmount: z.number().optional(),
-    taxPercentage: z.number().optional(),
+    taxPercentage: z.number().default(0),
     sentDate: z.string().datetime().nullish(),
     dueDate: z.string().datetime().nullish(),
   }),
