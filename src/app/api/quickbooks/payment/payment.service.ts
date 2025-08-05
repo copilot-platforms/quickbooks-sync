5a6ce923e07aa3efbdb8b4423a86bc7af629c992
SandipBajracharya <sndp106.sb@gmail.com>
2025-08-05 17:20:33 +0545

    update(OUT-2120): remove error message from sync log for invoice paid

--- src/app/api/quickbooks/payment/payment.service.ts
+++ src/app/api/quickbooks/payment/payment.service.ts
@@ -98,6 +98,7 @@ export class PaymentService extends BaseService {
           taxAmount: invoiceInfo.taxAmount,
           customerName: recipientInfo.displayName,
           customerEmail: recipientInfo.email,
+          errorMessage: '',
         },
       )
 
