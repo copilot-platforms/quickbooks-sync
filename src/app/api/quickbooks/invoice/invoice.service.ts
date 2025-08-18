370625e59a1591f94fa091be928bd6cb7bcae6b8
SandipBajracharya <sndp106.sb@gmail.com>
2025-08-18 18:33:16 +0545

    fix(OUT-2197): zod issue while making invoice paid

--- src/app/api/quickbooks/invoice/invoice.service.ts
+++ src/app/api/quickbooks/invoice/invoice.service.ts
@@ -804,7 +804,9 @@ export class InvoiceService extends BaseService {
       {
         invoiceNumber: payload.data.number,
         invoiceId: payload.data.id,
-        taxAmount: z.string().parse(invoiceLog.taxAmount),
+        taxAmount: invoiceLog.taxAmount
+          ? z.string().parse(invoiceLog.taxAmount)
+          : '0',
       },
       {
         displayName: customerDisplayName,
@@ -902,7 +904,9 @@ export class InvoiceService extends BaseService {
       ),
       this.logSync(payload.id, invoiceSync, EventType.VOIDED, {
         amount: z.string().parse(invoiceLog.amount),
-        taxAmount: z.string().parse(invoiceLog.taxAmount),
+        taxAmount: invoiceLog.taxAmount
+          ? z.string().parse(invoiceLog.taxAmount)
+          : '0',
         customerName: recipientInfo.displayName,
         customerEmail: recipientInfo.email,
       }),
@@ -986,7 +990,9 @@ export class InvoiceService extends BaseService {
       ),
       this.logSync(payload.id, syncedInvoice, EventType.DELETED, {
         amount: z.string().parse(invoiceLog.amount),
-        taxAmount: z.string().parse(invoiceLog.taxAmount),
+        taxAmount: invoiceLog.taxAmount
+          ? z.string().parse(invoiceLog.taxAmount)
+          : '0',
         customerName: recipientInfo.displayName,
         customerEmail: recipientInfo.email,
       }),
