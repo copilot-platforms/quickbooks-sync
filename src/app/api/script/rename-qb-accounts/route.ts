92c0ed57e475e1989652858249a8da4d2bf9b2b0
SandipBajracharya <sndp106.sb@gmail.com>
2025-12-25 16:18:53 +0545

    refactor(OUT-2473): remove api endpoint and create cmd to rename accounts

--- src/app/api/script/rename-qb-accounts/route.ts
+++
@@ -1,6 +0,0 @@
-import { withErrorHandler } from '@/app/api/core/utils/withErrorHandler'
-import { renameQbAccountName } from '@/app/api/script/rename-qb-accounts/renameQbAccount.controller'
-
-export const maxDuration = 800 // 13 minutes. Docs: https://vercel.com/docs/functions/configuring-functions/duration#duration-limits
-
-export const GET = withErrorHandler(renameQbAccountName)
